a133f3bc4e045bbdd3fc59f2a9d38083665f503c
sphma <sophia.ma13@utexas.edu>
2025-04-22 18:58:06 -0500

    asgardeo

--- frontend/my-react-app/src/config/asgardeoConfig.js
+++ frontend/my-react-app/src/config/asgardeoConfig.js
@@ -1,6 +1,6 @@
 export const config = {
-    signInRedirectURL: "http://localhost:5173/",
-    signOutRedirectURL: "http://localhost:5173/",
+    signInRedirectURL: import.meta.env.VITE_SIGN_IN_REDIRECT_URL,
+    signOutRedirectURL: import.meta.env.VITE_SIGN_OUT_REDIRECT_URL,
     clientID: "7mSV1WJWFmPbtoAofdMaHPRT420a",
     baseUrl: "https://api.asgardeo.io/t/smafs",
     scope: ["openid", "profile"]
