d4910536e8a2f6ae4b84ad22b1f59177705f345b
sphma <sophia.ma13@utexas.edu>
2025-04-22 19:02:35 -0500

    env fix

--- frontend/my-react-app/src/config/asgardeoConfig.js
+++ frontend/my-react-app/src/config/asgardeoConfig.js
@@ -1,6 +1,6 @@
 export const config = {
-    signInRedirectURL: import.meta.env.VITE_SIGN_IN_REDIRECT_URL,
-    signOutRedirectURL: import.meta.env.VITE_SIGN_OUT_REDIRECT_URL,
+    signInRedirectURL: import.meta.env.VITE_SIGNIN_URL,
+    signOutRedirectURL: import.meta.env.VITE_SIGNOUT_URL,
     clientID: "7mSV1WJWFmPbtoAofdMaHPRT420a",
     baseUrl: "https://api.asgardeo.io/t/smafs",
     scope: ["openid", "profile"]
